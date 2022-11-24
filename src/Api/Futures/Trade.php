b525609a35edc9b7e9757690184616211a0859d3
acr <acarbey15@gmail.com>
2022-11-24 16:50:36 +0300

    add new function

--- src/Api/Futures/Trade.php
+++ src/Api/Futures/Trade.php
@@ -10,135 +10,196 @@ use Lin\Binance\Request;
 class Trade extends Request
 {
     //Default required HMAC SHA256
-    protected $signature=true;
+    protected $signature = true;
 
     //Default seting
-    protected $version='v1';
+    protected $version = 'v1';
 
     function __construct(array $data)
     {
         parent::__construct($data);
 
-        $this->data['timestamp']=time().'000';
+        $this->data['timestamp'] = time() . '000';
     }
 
     /*
      *GET /fapi/v1/positionSide/dual (HMAC SHA256)
      */
-    public function getPositionSideDual(array $data=[]){
-        $this->type='get';
-        $this->path='/fapi/'.$this->version.'/positionSide/dual';
-        $this->data=array_merge($this->data,$data);
+    public function getPositionSideDual(array $data = [])
+    {
+        $this->type = 'get';
+        $this->path = '/fapi/' . $this->version . '/positionSide/dual';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 
     /*
      * GET /fapi/v1/multiAssetsMargin (HMAC SHA256)
      */
-    public function getMultiAssetsMargin(array $data=[]){
-        $this->type='get';
-        $this->path='/fapi/'.$this->version.'/multiAssetsMargin';
-        $this->data=array_merge($this->data,$data);
+    public function getMultiAssetsMargin(array $data = [])
+    {
+        $this->type = 'get';
+        $this->path = '/fapi/' . $this->version . '/multiAssetsMargin';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 
     /*
      *POST /fapi/v1/order (HMAC SHA256)
      */
-    public function postOrder(array $data=[]){
-        $this->type='POST';
-        $this->path='/fapi/'.$this->version.'/order';
-        $this->data=array_merge($this->data,$data);
+    public function postOrder(array $data = [])
+    {
+        $this->type = 'POST';
+        $this->path = '/fapi/' . $this->version . '/order';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 
     /*
      *POST /fapi/v1/batchOrders (HMAC SHA256)
      */
-    public function postBatchOrders(array $data=[]){
-        $this->type='POST';
-        $this->path='/fapi/'.$this->version.'/batchOrders';
-        $this->data=array_merge($this->data,$data);
+    public function postBatchOrders(array $data = [])
+    {
+        $this->type = 'POST';
+        $this->path = '/fapi/' . $this->version . '/batchOrders';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
+
+    public function openOrder(array $data = [])
+    {
+        $this->type = 'GET';
+        $this->path = '/fapi/' . $this->version . '/openOrder';
+        $this->data = array_merge($this->data, $data);
+        return $this->exec();
+    }
+
+    public function order(array $data = [])
+    {
+        $this->type = 'GET';
+        $this->path = '/fapi/' . $this->version . '/order';
+        $this->data = array_merge($this->data, $data);
+        return $this->exec();
+    }
+
+    public function allOrders(array $data = [])
+    {
+        $this->type = 'GET';
+        $this->path = '/fapi/' . $this->version . '/allOrders';
+        $this->data = array_merge($this->data, $data);
+        return $this->exec();
+    }
+
+    public function openOrders(array $data = [])
+    {
+        $this->type = 'GET';
+        $this->path = '/fapi/' . $this->version . '/openOrders';
+        $this->data = array_merge($this->data, $data);
+        return $this->exec();
+    }
+
+    public function userTrades(array $data = [])
+    {
+        $this->type = 'GET';
+        $this->path = '/fapi/' . $this->version . '/userTrades';
+        $this->data = array_merge($this->data, $data);
+        return $this->exec();
+    }
+
 
     /*
      *DELETE /fapi/v1/order (HMAC SHA256)
      */
-    public function deleteOrder(array $data=[]){
-        $this->type='DELETE';
-        $this->path='/fapi/'.$this->version.'/order';
-        $this->data=array_merge($this->data,$data);
+    public function deleteOrder(array $data = [])
+    {
+        $this->type = 'DELETE';
+        $this->path = '/fapi/' . $this->version . '/order';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 
     /*
      *DELETE /fapi/v1/allOpenOrders (HMAC SHA256)
      */
-    public function deleteAllOpenOrders(array $data=[]){
-        $this->type='DELETE';
-        $this->path='/fapi/'.$this->version.'/allOpenOrders';
-        $this->data=array_merge($this->data,$data);
+    public function deleteAllOpenOrders(array $data = [])
+    {
+        $this->type = 'DELETE';
+        $this->path = '/fapi/' . $this->version . '/allOpenOrders';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 
     /*
      *DELETE /fapi/v1/batchOrders (HMAC SHA256)
      */
-    public function deleteBatchOrders(array $data=[]){
-        $this->type='DELETE';
-        $this->path='/fapi/'.$this->version.'/batchOrders';
-        $this->data=array_merge($this->data,$data);
+    public function deleteBatchOrders(array $data = [])
+    {
+        $this->type = 'DELETE';
+        $this->path = '/fapi/' . $this->version . '/batchOrders';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 
     /*
      *POST /fapi/v1/countdownCancelAll (HMAC SHA256)
      */
-    public function postCountdownCancelAll(array $data=[]){
-        $this->type='POST';
-        $this->path='/fapi/'.$this->version.'/countdownCancelAll';
-        $this->data=array_merge($this->data,$data);
+    public function postCountdownCancelAll(array $data = [])
+    {
+        $this->type = 'POST';
+        $this->path = '/fapi/' . $this->version . '/countdownCancelAll';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 
     /*
      *POST /fapi/v1/leverage (HMAC SHA256)
      */
-    public function postLeverage(array $data=[]){
-        $this->type='post';
-        $this->path='/fapi/'.$this->version.'/leverage';
-        $this->data=array_merge($this->data,$data);
+    public function postLeverage(array $data = [])
+    {
+        $this->type = 'post';
+        $this->path = '/fapi/' . $this->version . '/leverage';
+        $this->data = array_merge($this->data, $data);
+        return $this->exec();
+    }
+
+    public function getMaxLeverage(array $data = [])
+    {
+        $this->type = 'get';
+        $this->path = '/fapi/' . $this->version . '/leverageBracket';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 
     /*
      *POST /fapi/v1/marginType (HMAC SHA256)
      */
-    public function getMarginType(array $data=[]){
-        $this->type='POST';
-        $this->path='/fapi/'.$this->version.'/marginType';
-        $this->data=array_merge($this->data,$data);
+    public function getMarginType(array $data = [])
+    {
+        $this->type = 'POST';
+        $this->path = '/fapi/' . $this->version . '/marginType';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 
     /*
      *POST /fapi/v1/positionMargin (HMAC SHA256)
      */
-    public function postPositionMargin(array $data=[]){
-        $this->type='POST';
-        $this->path='/fapi/'.$this->version.'/positionMargin';
-        $this->data=array_merge($this->data,$data);
+    public function postPositionMargin(array $data = [])
+    {
+        $this->type = 'POST';
+        $this->path = '/fapi/' . $this->version . '/positionMargin';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 
     /*
      *GET /fapi/v1/positionMargin/history (HMAC SHA256)
      */
-    public function getPositionMarginHistory(array $data=[]){
-        $this->type='get';
-        $this->path='/fapi/'.$this->version.'/positionMargin/history';
-        $this->data=array_merge($this->data,$data);
+    public function getPositionMarginHistory(array $data = [])
+    {
+        $this->type = 'get';
+        $this->path = '/fapi/' . $this->version . '/positionMargin/history';
+        $this->data = array_merge($this->data, $data);
         return $this->exec();
     }
 }
